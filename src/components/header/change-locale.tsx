a2aaee1af9b113e1590a5e4ee2af70de2f28c55a
Toby Chan <tobyshchan@gmail.com>
2022-12-26 22:55:01 +0800

    update styling

--- src/components/header/change-locale.tsx
+++ src/components/header/change-locale.tsx
@@ -5,10 +5,10 @@ export const ChangeLocale = component$(() => {
   const ctx = useSpeakContext();
 
   return (
-    <div>
-      <div>{t("app.changeLocale@@Change locale")}</div>
+    <div class="flex items-center">
       {ctx.config.supportedLocales.map((locale) => (
         <button
+          class="inline-flex items-center rounded border border-transparent bg-indigo-600 px-2.5 py-1.5 text-xs font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
           onClick$={async () => await changeLocale(locale, ctx)}
           key={locale.lang}
         >
