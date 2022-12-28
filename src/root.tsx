5ea2757d9962a04748335de1c72184395b1a882b
Toby Chan <renascence0423@gmail.com>
2022-12-28 17:30:42 +0800

    npm install firebase

--- src/root.tsx
+++ src/root.tsx
@@ -1,14 +1,14 @@
-import { component$ } from '@builder.io/qwik';
+import { component$ } from '@builder.io/qwik'
 import {
   QwikCityProvider,
   RouterOutlet,
   ServiceWorkerRegister,
-} from '@builder.io/qwik-city';
-import { QwikSpeak } from 'qwik-speak';
-import { RouterHead } from './components/router-head/router-head';
-
-import './global.css';
-import { config, translationFn } from './speak-config';
+} from '@builder.io/qwik-city'
+import { QwikSpeak } from 'qwik-speak'
+import { RouterHead } from './components/router-head/router-head'
+import { QwikPartytown } from './components/partytown/partytown'
+import './global.css'
+import { config, translationFn } from './speak-config'
 
 export default component$(() => {
   /**
@@ -32,27 +32,27 @@ export default component$(() => {
     <QwikSpeak config={config} translationFn={translationFn}>
       <QwikCityProvider>
         <head>
-          <meta name='viewport' content='width=device-width,initial-scale=1' />
-          <meta name='description' content='Messing around qwik' />
-          <link rel='icon' href='/favicon.ico' />
-          <script src='/registerSW.js' />
+          <QwikPartytown forward={['dataLayer.push']} />;
+          <meta name="viewport" content="width=device-width,initial-scale=1" />
+          <meta name="description" content="Messing around qwik" />
+          <link rel="icon" href="/favicon.ico" />
+          <script src="/registerSW.js" />
           <link
-            rel='apple-touch-icon'
-            href='/apple-touch-icon.png'
-            sizes='180x180'
+            rel="apple-touch-icon"
+            href="/apple-touch-icon.png"
+            sizes="180x180"
           />
-          <link rel='mask-icon' href='/mask-icon.svg' color='#FFFFFF' />
-          <meta name='theme-color' content='#ffffff' />
-
-          <meta charSet='utf-8' />
-          <link rel='manifest' href='/manifest.webmanifest' />
+          <link rel="mask-icon" href="/mask-icon.svg" color="#FFFFFF" />
+          <meta name="theme-color" content="#ffffff" />
+          <meta charSet="utf-8" />
+          <link rel="manifest" href="/manifest.webmanifest" />
           <RouterHead />
         </head>
-        <body lang='en'>
+        <body lang="en">
           <RouterOutlet />
           <ServiceWorkerRegister />
         </body>
       </QwikCityProvider>
     </QwikSpeak>
-  );
-});
+  )
+})
