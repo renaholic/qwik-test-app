d2e1709d9263864e409a2ceda8d2aa3dc65df748
Toby Chan <tobyshchan@gmail.com>
2022-12-31 22:16:18 +0800

    attempt to fix edge render

--- src/routes/yt-playback/index.tsx
+++ src/routes/yt-playback/index.tsx
@@ -57,61 +57,51 @@ export default component$(() => {
     prompt: '',
   })
 
-  useClientEffect$(
-    () => {
-      const { videoID, videoTimestamp, playAt } = store
+  useClientEffect$(() => {
+    const { videoID, videoTimestamp, playAt } = store
 
-      let player: ReturnType<typeof YouTubePlayer> | null
-      let timer: ReturnType<typeof setTimeout> | null
+    if (!videoID || !videoTimestamp || !playAt) return
 
-      if (videoID && videoTimestamp && playAt) {
-        const timeOffset = dayjs(new Date(store.playAt)).subtract(
-          store.videoTimestamp,
-          'seconds'
-        )
-        const timeDiff = timeOffset.diff(dayjs(), 'milliseconds')
+    const timeOffset = dayjs(new Date(store.playAt)).subtract(
+      store.videoTimestamp,
+      'seconds'
+    )
+    const timeDiff = timeOffset.diff(dayjs(), 'milliseconds')
+    const player = YouTubePlayer('player', {
+      videoId: videoID,
 
-        if (timeDiff < 0 && Math.abs(timeDiff / 1000) > store.videoTimestamp) {
-          store.prompt = `The ship has completely sailed`
-          return
-        }
-        const player = YouTubePlayer('player', {
-          videoId: videoID,
+      playerVars: {
+        start: timeDiff <= 0 ? Math.abs(Math.floor(timeDiff / 1000)) : 0,
+        autoplay: timeDiff <= 0 ? 1 : 0,
+        origin: isDev ? 'http://localhost:5173' : 'https://qwik.alepholic.dev',
+        enablejsapi: 1,
+      },
+    })
 
-          playerVars: {
-            start: timeDiff < 0 ? Math.abs(Math.floor(timeDiff / 1000)) : 0,
-            autoplay: timeDiff < 0 ? 1 : 0,
-            origin: isDev
-              ? 'http://localhost:5173'
-              : 'https://qwik.alepholic.dev',
-            enablejsapi: 1,
-          },
-        })
+    if (timeDiff < 0 && Math.abs(timeDiff / 1000) > store.videoTimestamp) {
+      store.prompt = `The ship has completely sailed`
+      return
+    }
 
-        console.log({ diff: timeDiff / 1000 })
+    if (timeDiff < 0) {
+      store.prompt = `The 00:00 ship has sailed, starting at ${dayjs
+        .duration(Math.floor(Math.abs(timeDiff / 1000)), 'seconds')
+        .format('mm:ss')} seconds ahead instead`
+    } else {
+      store.prompt = `Video will start at ${timeOffset.format(
+        'YYYY-MM-DDTHH:mm:ss'
+      )}`
+    }
 
-        if (timeDiff < 0) {
-          store.prompt = `The 00:00 ship has sailed, starting at ${dayjs
-            .duration(Math.floor(Math.abs(timeDiff / 1000)), 'seconds')
-            .format('mm:ss')} seconds ahead instead`
-        } else {
-          store.prompt = `Video will start at ${timeOffset.format(
-            'YYYY-MM-DDTHH:mm:ss'
-          )}`
-        }
+    const timer = setTimeout(async () => {
+      await player.playVideo()
+    }, timeDiff)
 
-        timer = setTimeout(async () => {
-          console.log('play')
-          await player.playVideo()
-        }, timeDiff)
-      }
-      return () => {
-        player && player.destroy()
-        timer && clearTimeout(timer)
-      }
-    },
-    { eagerness: 'visible' }
-  )
+    return () => {
+      player && player.destroy()
+      timer && clearTimeout(timer)
+    }
+  })
 
   return (
     <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
