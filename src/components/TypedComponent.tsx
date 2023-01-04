0832a26cf144d14fe38fc2561599fd9bf39e8393
Toby Chan <renascence0423@gmail.com>
2023-01-05 01:33:14 +0800

    make the component as div

--- src/components/TypedComponent.tsx
+++ src/components/TypedComponent.tsx
@@ -1,24 +1,40 @@
-import { component$, useSignal, useClientEffect$ } from '@builder.io/qwik'
+import { component$, useSignal, useClientEffect$, Slot } from '@builder.io/qwik'
 import Typed from 'typed.js'
 
-export const TypedComponent = component$(({ strings = [] }: { strings?: string[] }) => {
-  const ref = useSignal<HTMLSpanElement | undefined>()
-  useClientEffect$(
-    () => {
-      if (!ref.value) return
-      const typed = new Typed(ref.value, {
-        strings,
-        typeSpeed: 80,
-        backDelay: 1000,
-        backSpeed: 100,
-        loop: true,
-        smartBackspace: true,
-      })
-      return () => typed.destroy()
-    },
-    {
-      eagerness: 'visible',
-    }
-  )
-  return <span ref={ref} />
-})
+export const TypedComponent = component$(
+  ({ strings = [] }: { strings?: string[] }) => {
+    const ref = useSignal<HTMLSpanElement | undefined>()
+
+    useClientEffect$(
+      () => {
+        if (!ref.value) return
+        const typed = new Typed(ref.value, {
+          strings,
+          // stringsElement: '#typed-strings',
+          typeSpeed: 80,
+          backDelay: 1000,
+          backSpeed: 100,
+          loop: true,
+          smartBackspace: true,
+        })
+        return () => typed.destroy()
+      },
+      {
+        eagerness: 'visible',
+      }
+    )
+    return (
+      <div>
+        {/* <div id="typed-strings">
+          <p>
+            Typed.js is a <strong>JavaScript</strong> library.
+          </p>
+          <p>
+            It <em>types</em> out sentences.
+          </p>
+        </div> */}
+        <span ref={ref} class={'whitespace-pre'} />
+      </div>
+    )
+  }
+)
