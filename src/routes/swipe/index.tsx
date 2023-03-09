26a83a7dade9b23e840bbb88d611b90eee50e1a1
Toby Chan <renascence0423@gmail.com>
2023-03-09 16:00:03 +0800

    extract the draggable into single page

---
+++ src/routes/swipe/index.tsx
@@ -0,0 +1,112 @@
+import {
+  component$,
+  useSignal,
+  useBrowserVisibleTask$,
+  useTask$,
+  useStore,
+  NoSerialize,
+} from '@builder.io/qwik'
+import { DocumentHead } from '@builder.io/qwik-city'
+import { isBrowser } from '@builder.io/qwik/build'
+import { DragGesture } from '@use-gesture/vanilla'
+import anime from 'animejs'
+import { Speak } from 'qwik-speak'
+import { twMerge } from 'tailwind-merge'
+import { usePageContext } from '../../root'
+
+export const DraggableDiv = component$(() => {
+  usePageContext('gesture and animejs')
+
+  const eleRef = useSignal<Element>()
+
+  const isActive = useSignal(false)
+
+  const isGone = useSignal(false)
+  const store = useStore<{
+    time: null | string
+    cleanup: NoSerialize<() => void>
+    animateInstance: NoSerialize<anime.AnimeInstance>
+  }>({
+    time: null,
+    cleanup: undefined,
+    animateInstance: undefined,
+  })
+
+  useBrowserVisibleTask$(async ({ track }) => {
+    track(() => eleRef)
+
+    if (!eleRef.value) return
+
+    const gesture = new DragGesture(
+      eleRef.value,
+      ({
+        args: [index],
+        active,
+        movement: [mx, my],
+        direction: [xDir],
+        velocity: [vx],
+      }) => {
+        const isFlick = vx > 0.2 // If you flick hard enough it should trigger the card to fly out
+        if (!active && isFlick) isGone.value = true // If button/finger's up and trigger velocity is reached, we flag the card ready to fly out
+
+        const x = isGone.value
+          ? (200 + window.innerWidth) * xDir
+          : active
+          ? mx
+          : 0 // When a card is gone it flys out left or right, otherwise goes back to zero
+        const rotate = mx / 100 + (isGone ? xDir * 3 * vx : 0) // How much the card tilts, flicking it harder makes it rotate faster
+
+        const scale = active ? 1.1 : 1 // Active cards lift up a bit
+
+        anime({
+          targets: eleRef.value,
+          translateX: x,
+          translateY: active ? my : 0,
+          duration: active ? 0 : 1000,
+          elasticity: 600,
+
+          scale,
+          rotate,
+        })
+      },
+      {
+        axis: 'x',
+      }
+    )
+
+    return () => gesture.destroy()
+  })
+
+  return (
+    <div
+      ref={eleRef}
+      class={twMerge(
+        'touch-pan-y select-none',
+        'h-9 bg-gray-800',
+        'text-2xl text-gray-300',
+        'flex items-center justify-center',
+        'rounded-md shadow-md'
+      )}
+    >
+      {isActive.value ? <p>I am moving! 🏎️</p> : <p>← Drag Me! →</p>}
+    </div>
+  )
+})
+
+export default component$(() => {
+  return (
+    <Speak assets={['home']}>
+      <a href="https://use-gesture.netlify.app/" target="_blank">
+        use-gesture
+      </a>
+      <a href="https://animejs.com/" target="_blank">
+        animejs
+      </a>
+      <DraggableDiv />
+    </Speak>
+  )
+})
+
+export const head: DocumentHead = {
+  title: 'use-gesture and animejs',
+}
