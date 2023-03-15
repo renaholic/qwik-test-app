77c5ad208f545fb57bfaa954432e4e184c50b0ab
Toby Chan <renascence0423@gmail.com>
2023-03-15 20:57:59 +0800

    remove comment code

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -48,14 +48,8 @@ export default component$(() => {
         <input id="my-drawer" type="checkbox" class="drawer-toggle" />
         <div class="drawer-content">
           {/* <!-- Page content here --> */}
-          {/* <label for="my-drawer" class="btn btn-primary drawer-button">
-            Open drawer
-          </label> */}
           <div class="navbar bg-base-100" id="nav">
             <div class="navbar-start">
-              {/* <label for="my-drawer" class="btn btn-primary drawer-button">
-                Open drawer
-              </label> */}
               <label
                 tabIndex={0}
                 for="my-drawer"
